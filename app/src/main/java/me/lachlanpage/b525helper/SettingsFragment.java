d1f37bc1717fbff23d4f4b9e2185476f86d178df
lachlanpage <lachlanpage97@gmail.com>
2019-02-27 20:00:28 +1030

    Update preferences keys and listeners

--- app/src/main/java/me/lachlanpage/b525helper/SettingsFragment.java
+++ app/src/main/java/me/lachlanpage/b525helper/SettingsFragment.java
@@ -1,5 +1,6 @@
 package me.lachlanpage.b525helper;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.preference.EditTextPreference;
@@ -19,15 +20,24 @@ public class SettingsFragment extends PreferenceFragmentCompat{
         setPreferencesFromResource(R.xml.preferences, rootKey);
 
 
-        // LISTENERS FOR THE 3 SETTINGS
-        final EditTextPreference modemIPPref = (EditTextPreference)findPreference("modemIP");
-        modemIPPref.setSummary(modemIPPref.getText());
-        modemIPPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                final EditTextPreference pref = (EditTextPreference)findPreference("username");
+        // Listener for feedback click
+        final Preference feedbackPref = (Preference) findPreference("feedback");
+        feedbackPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
 
-                pref.setSummary(pref.getText());
-                return true;
+                /* Create the Intent */
+                final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+
+                /* Fill it with Data */
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"lachlanpage97@gmail.com"});
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Feedback");
+                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+
+                /* Send it off to the Activity-Chooser */
+                getActivity().startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+                return false;
             }
         });
     }
